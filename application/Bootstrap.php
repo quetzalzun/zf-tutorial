5e4e70cabd5fc6cb79c514f917b88e2010d3867d
Simon Westcott <swestcott@gmail.com>
2009-05-20 23:30:40 +0100

    Added magic autoloader method to bootstrap class

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,13 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    protected function _initAutoload()
+    {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => '',
+            'basePath'  => APPLCIATION_PATH));
+        return $moduleLoader;
+    }
 
 }
 
