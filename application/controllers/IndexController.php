a6c993d0004f3901c827370b15e45ef93a880623
Simon Westcott <swestcott@gmail.com>
2009-05-25 09:39:58 +0100

    Started populating IndexController

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,6 +10,11 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
+		$this->view->title = 'My Albums';
+		$this->view->headTitle($this->view->title, 'PREPEND');
+
+		$albums = new Model_DbTable_Albums();
+		$this->view->albums = $albums->fetchAll();
         // action body
     }
 
@@ -28,12 +33,4 @@ class IndexController extends Zend_Controller_Action
         // action body
     }
 
-
 }
-
-
-
-
-
-
-
