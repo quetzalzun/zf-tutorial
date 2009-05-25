41275ca81bc3a6c2bed0d68cb79482ea589df311
Simon Westcott <swestcott@gmail.com>
2009-05-25 15:03:47 +0100

    Part way through Index/edit - nearly @ train station

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -44,7 +44,33 @@ class IndexController extends Zend_Controller_Action
 
     public function editAction()
     {
-        // action body
+		$this->view->title = 'Edit Album';
+		$this->view->headTitle($this->view->title, 'PREPEND');
+
+		$form = new Form_Album();
+		$form->submit->setLabel('Save');
+		$this->view->form = $form;
+
+		if($this->getRequest()->isPost()) {
+			$formData = $this->getRequest()->getPost();
+			if($form->isValid($formData)) {
+				$id = (int)$form->getValue('id');
+				$artist = $form->getValue('artist');
+				$title = $form->getValue('title');
+				$albums = new Model_DbTable_Albums();
+				$albums->updateAlbum($id, $artist, $title);
+
+				$this->_redirect('/');
+			} else {
+				$form->populate($formData);
+			}
+		} else {
+			$id = $this->_getParam('id', 0);
+			if($id > 0) {
+				$albums = new Model_DbTable_Albums();
+				$form->populate($albums->gettAlbum($id));
+			}
+		}
     }
 
     public function deleteAction()
